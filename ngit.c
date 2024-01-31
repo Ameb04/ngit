256798856397c460d35adf828a72ea54a33e005c
amirm.eb <amirm.eb>
2024-02-01 00:12:01 +0330

    in final of compliting add

--- ngit.c
+++ ngit.c
@@ -9,41 +9,154 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <io.h>
-#include <unistd.h>
+#include <time.h>
 #define debug(x) printf("*%s*\n", x);
+#define barresi printf("dorost\n");
 #define MAX_SIZE 1000
 char *project_path;
-void copy_file(const char *src_path, const char *dst_path)
+char *find_file_path(char *dir, char *file_name)
 {
-    FILE *src_file = fopen(src_path, "rb");
-    if (src_file == NULL)
+    DIR *dp = opendir(dir);
+    if (dp == NULL)
     {
-        perror("fopen src_file");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
-    FILE *dst_file = fopen(dst_path, "wb");
-    if (dst_file == NULL)
+    char *path = malloc(MAX_PATH);
+    if (path == NULL)
     {
-        perror("fopen dst_file");
-        exit(EXIT_FAILURE);
+        closedir(dp);
+        return NULL;
     }
-    char buffer[4096];
-    size_t n;
-    while ((n = fread(buffer, 1, sizeof(buffer), src_file)) > 0)
+    struct dirent *entry;
+    while ((entry = readdir(dp)) != NULL)
     {
-        if (fwrite(buffer, 1, n, dst_file) != n)
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
         {
-            perror("fwrite");
-            exit(EXIT_FAILURE);
+            continue;
+        }
+        sprintf(path, "%s\\%s", dir, entry->d_name);
+        struct stat statbuf;
+        if (stat(path, &statbuf) == 0)
+        {
+            if (S_ISREG(statbuf.st_mode))
+            {
+                if (strcmp(entry->d_name, file_name) == 0)
+                {
+                    closedir(dp);
+                    return path;
+                }
+            }
+            else if (S_ISDIR(statbuf.st_mode))
+            {
+                char *sub_path = find_file_path(path, file_name);
+                if (sub_path != NULL)
+                {
+                    free(path);
+                    closedir(dp);
+                    return sub_path;
+                }
+            }
         }
     }
-    if (ferror(src_file))
+    free(path);
+    closedir(dp);
+    return NULL;
+}
+char *strrev(char *str)
+{
+    int len = strlen(str);
+    char *rev = malloc(len + 1);
+    rev[len] = '\0';
+    for (int i = 0; i < len; i++)
     {
-        perror("fread");
-        exit(EXIT_FAILURE);
+        rev[i] = str[len - i - 1];
     }
-    fclose(src_file);
-    fclose(dst_file);
+    return rev;
+}
+int compare_files(char *path1, char *path2)
+{
+    HANDLE handle1 = CreateFile(path1, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (handle1 == INVALID_HANDLE_VALUE)
+    {
+        printf("Error: Unable to open file %s\n", path1);
+        return -2;
+    }
+    HANDLE handle2 = CreateFile(path2, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (handle2 == INVALID_HANDLE_VALUE)
+    {
+        printf("Error: Unable to open file %s\n", path2);
+        return -2;
+    }
+    FILETIME last_write_time1, last_write_time2;
+    BOOL result1 = GetFileTime(handle1, NULL, NULL, &last_write_time1);
+    BOOL result2 = GetFileTime(handle2, NULL, NULL, &last_write_time2);
+    if (result1 == FALSE || result2 == FALSE)
+    {
+        printf("Error: Unable to get file times for %s and %s\n", path1, path2);
+        return -2;
+    }
+    CloseHandle(handle1);
+    CloseHandle(handle2);
+    // Use CompareFileTime to compare the two FILETIME values
+    // The function returns -1 if the first value is less than the second value
+    // The function returns 0 if the two values are equal
+    // The function returns 1 if the first value is greater than the second value
+    int comparison = CompareFileTime(&last_write_time1, &last_write_time2);
+    return comparison;
+}
+int check_folder_of_file_exist_in_now_and_subdirectories(char *path, char *name)
+{
+    char search_path[MAX_PATH];
+    sprintf(search_path, "%s\\*", path);
+    WIN32_FIND_DATA data;
+    HANDLE handle = FindFirstFile(search_path, &data);
+    if (handle == INVALID_HANDLE_VALUE)
+    {
+        printf("Error: Unable to open directory %s\n", path);
+        return -1;
+    }
+    do
+    {
+        if (strcmp(data.cFileName, name) == 0)
+        {
+            FindClose(handle);
+            return 1;
+        }
+        if (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+        {
+            if (strcmp(data.cFileName, ".") != 0 && strcmp(data.cFileName, "..") != 0)
+            {
+                char sub_path[MAX_PATH];
+                sprintf(sub_path, "%s\\%s", path, data.cFileName);
+                int result = check_folder_of_file_exist_in_now_and_subdirectories(sub_path, name);
+                if (result != 0)
+                {
+                    FindClose(handle);
+                    return result;
+                }
+            }
+        }
+    } while (FindNextFile(handle, &data));
+    FindClose(handle);
+    return 0;
+}
+void copy_file(const char *src_path, const char *dst_path)
+{
+    BOOL result = CopyFile(src_path, dst_path, FALSE);
+    if (result)
+    {
+        printf("File copied successfully.\n");
+    }
+    else
+    {
+        printf("File copy failed. Error: %u\n", GetLastError());
+    }
+    char command[MAX_SIZE];
+    strcpy(command, "xcopy ");
+    strcat(command, src_path);
+    strcat(command, " ");
+    strcat(command, dst_path);
+    system(command);
 }
 void copy_folder(const char *src_path, const char *dst_path)
 {
@@ -235,6 +348,7 @@ void create_local_config(int argc, char **argv)
         FILE *locak_variables;
         locak_variables = fopen("C:\\newgit\\GlobalVariables.txt", "a");
         fprintf(locak_variables, "%s %s\n", argv[3], alias);
+        // must be checked that a alias exist or not
         free(alias);
     }
     else
@@ -258,7 +372,7 @@ void run_init(int argc, char **argv)
         fclose(project_path_adding);
         system("mkdir ngit");
         system("attrib +h ngit");
-        system("cd ngit & type nul > PersonalVariables.txt & type nul > PersonalUser.txt & type nul > StageCase.txt & echo master > CommitsAndBranches.txt");
+        system("cd ngit & type nul > PersonalVariables.txt & type nul > PersonalUser.txt & echo 1 > StageCase.txt & echo master > CommitsAndBranches.txt");
         copy_global_to_all();
     }
 }
@@ -268,39 +382,151 @@ void add_to_stage(int argc, char **argv)
     GetCurrentDirectory(sizeof(address), address);
     int is_project = dir_exist("ngit");
     SetCurrentDirectory(address);
-
+    char location[MAX_SIZE];
+    strcpy(location, project_path);
+    strcat(location, "\\ngit\\StageCase.txt");
+    FILE *stagecase = fopen(location, "r");
+    char is_commited[MAX_SIZE];
+    fscanf(stagecase, "%[^\0]", is_commited);
+    fclose(stagecase);
     if (is_project)
     {
-        int result;
-        FILE *stage_area = fopen("ngit\\StageCase.txt", "r");
-        char staged_files[MAX_SIZE];
-        fscanf(stage_area, "%[^\0]", staged_files);
-        fclose(stage_area);
-        for (int i = 2; i < argc; i++)
+        if (strncmp(is_commited, "1", 1) == 0)
         {
-            result = dir_exist(argv[i]);
-            if (result == 1)
+            srand(time(NULL));
+            int random_number = rand() % 100000 + 1234567;
+            stagecase = fopen(location, "w");
+            fprintf(stagecase, "%d %d", 0, random_number); // when commited must be 1
+            fclose(stagecase);
+            char random[MAX_SIZE];
+            sprintf(random, "%d", random_number);
+            strcpy(location, project_path);
+            strcat(location, "\\ngit");
+            SetCurrentDirectory(location);
+            mkdir(random);
+            SetCurrentDirectory(address);
+            int result = 0;
+            strcat(location, "\\");
+            strcat(location, random);
+            for (int i = 2; i < argc; i++)
             {
-                char *one_line = strtok(staged_files, "\n");
-                int exist = 0;
-                while (one_line != NULL)
+                int is_file = 0;
+                if (strchr(argv[i], '.') != NULL)
                 {
-                    if (strncmp(one_line, argv[i], strlen(argv[i])) == 0)
-                    {
-                        exist = 1;
-                    }
-                    one_line = strtok(NULL, "\n");
+                    is_file = 1;
                 }
-                if (!exist)
+                if (is_file)
                 {
-                    FILE *add_to_stagin_area = fopen("ngit\\StageCase.txt", "a");
-                    fprintf(add_to_stagin_area, "%s\n", argv[i]);
-                    fclose(add_to_stagin_area);
+                    if (access(argv[i], F_OK) == 0)
+                    {
+
+                        copy_file(argv[i], location);
+                    }
+                    else
+                    {
+                        printf("Error : file not found\n");
+                    }
+                }
+                else
+                {
+                    if (access(argv[i], F_OK) == 0)
+                    {
+                        copy_folder(argv[i], location);
+                    }
+                    else
+                    {
+                        printf("Error : folder not found\n");
+                    }
                 }
             }
-            else
+        }
+        else if (strncmp(is_commited, "0", 1) == 0)
+        {
+            char *reshte = strtok(is_commited, " ");
+            reshte = strtok(NULL, " ");
+            char random[MAX_SIZE] = "\\";
+            sprintf(random + 1, "%s", reshte);
+            strcpy(location, project_path);
+            strcat(location, "\\ngit\\");
+            strcat(location, is_commited + 2);
+            int result = 0;
+            for (int i = 2; i < argc; i++)
             {
-                printf("Error : file not found\n");
+                int is_file = 0;
+                if (strchr(argv[i], '.') != NULL)
+                {
+                    is_file = 1;
+                }
+                if (is_file)
+                {
+                    int added_in_previos = check_folder_of_file_exist_in_now_and_subdirectories(location, argv[i]);
+                    if (added_in_previos)
+                    {
+                        char *argv_copy = malloc(MAX_SIZE);
+                        strcpy(argv_copy, argv[i]);
+                        argv_copy = strrev(argv_copy);
+                        char *token = strtok(argv_copy, "\\");
+                        if (token == NULL)
+                        {
+                            strcpy(argv_copy, argv[i]);
+                        }
+                        else
+                        {
+                            argv_copy = strrev(argv_copy);
+                        }
+                        char absolute_file_path[MAX_PATH];
+                        DWORD length;
+                        LPTSTR fileName;
+                        length = GetFullPathName(argv_copy, MAX_PATH, absolute_file_path, &fileName);
+                        debug(location);
+                        debug(argv_copy);
+                        char *file_full_path = find_file_path(location, argv_copy);
+                        if (length == 0)
+                        {
+                            printf("Error: %lu\n", GetLastError());
+                        }
+                        else
+                        {
+                            debug(file_full_path);
+                            debug(absolute_file_path);
+                            int is_change = compare_files(file_full_path, absolute_file_path);
+                            if (is_change != 0)
+                            {
+                                if (access(argv[i], F_OK) == 0)
+                                {
+                                    copy_file(absolute_file_path, file_full_path);
+                                }
+                            }
+                        }
+                        free(argv_copy);
+                    }
+                    else
+                    {
+                        if (access(argv[i], F_OK) == 0)
+                        {
+                            copy_file(argv[i], location);
+                        }
+                    }
+                    // if (access(argv[i], F_OK) == 0)
+                    // {
+                    //     copy_file(argv[i], location);
+                    // }
+                    // else
+                    // {
+                    //     printf("Error : file not found\n");
+                    // }
+                }
+                else
+                {
+                    if (access(argv[i], F_OK) == 0)
+                    {
+                        copy_folder(argv[i], location);
+                    }
+                    else
+                    {
+                        printf("Error : folder not found\n");
+                    }
+                }
             }
         }
     }
@@ -340,7 +566,7 @@ int main(int argc, char **argv)
         {
             run_init(argc, argv);
         }
-        else if ((strncmp(argv[1], "add", 3) == 0) && argc > 2)
+        else if ((strncmp(argv[1], "add", 3) == 0) && argc > 2) // must code -f and -n and function need to be change in i
         {
             add_to_stage(argc, argv);
         }
@@ -352,4 +578,6 @@ int main(int argc, char **argv)
     {
         printf("Error : command not found. please put \"ngit\" in first of your command and type the rest of command.\n");
     }
+    free(project_path);
+    return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,1 @@
-fclose(add_to_stagin_area);
+debug(reshte);
